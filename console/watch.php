dc4f628a882393681dc94db075b30ae88b2d4dab
Jochen Timmermans <jochentimmermans@gmail.com>
2020-05-16 18:27:45 +0200

    first commit

---
+++ console/watch.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * sass:watch command for Skeleton Console
+ *
+ * @author Jochen Timmermans <jochen@tigron.be>
+ */
+
+namespace Skeleton\Console\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Sass_Watch extends \Skeleton\Console\Command {
+
+	/**
+	 * Configure the Create command
+	 *
+	 * @access protected
+	 */
+	protected function configure() {
+		$this->setName('sass:watch');
+		$this->setDescription('Start SASS watcher');
+	}
+
+	/**
+	 * Execute the Command
+	 *
+	 * @access protected
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		if (!file_exists(\Skeleton\Sass\Config::$watch_directory)) {
+			$output->writeln('<error>Config::$watch_directory is not set to a valid directory</error>');
+			return 1;
+		}
+
+		$watch_directory = \Skeleton\Test\Config::$watch_directory;
+		$output_directory = \Skeleton\Test\Config::$output_directory;
+
+		echo "Watch directory: " . $watch_directory . " - " . $output_directory;
+
+//		return 0;
+	}
+
+}
---
+++ lib/Skeleton/Sass/Config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Config class
+ *
+ * @author Jochen Timmermans <jochen@tigron.be>
+ */
+
+namespace Skeleton\Sass;
+
+class Config {
+
+	/**
+	 * Watch directory
+	 *
+	 * This watches the directory where the SASS files are stored.
+	 *
+	 * @access public
+	 * @var string $watch_directory
+	 */
+	public static $watch_directory = null;
+
+	/**
+	 * Output directory
+	 *
+	 * This watches the directory where the SASS files are stored.
+	 *
+	 * @access public
+	 * @var string $output_directory
+	 */
+	public static $output_directory = null;
+
+}
